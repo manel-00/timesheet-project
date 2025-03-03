8a4d3e24c4e43a22c5ff60781bda4e84e3b2bd8f
manel-00 <manel.fatnassi@esprit.tn>
2025-03-03 11:31:34 +0000

    chap 6

--- src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -7,10 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 class UserServiceImplTest {
 
-    @Disabled("This test is disabled and should not run.")
+   // @Disabled("This test is disabled and should not run.")
     @Test
     void testAddUser() {
         String envVar = System.getenv("TIMESHEET_TESTS_FAIL");
+
+   
+
+
+  
+
         if ("True".equalsIgnoreCase(envVar)) {
             fail("Le test échoue car TIMESHEET_TESTS_FAIL est activé !");
         }
